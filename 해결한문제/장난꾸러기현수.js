01f7c271a94187d7044a864af5dd5ddc62af75c1
bjc1102 <bjc30849@gmail.com>
2022-10-12 15:32:37 +0900

    JS sort: 장난꾸러기현수

---
+++ 해결한문제/장난꾸러기현수.js
@@ -0,0 +1,22 @@
+function solution(arr) {
+  /*
+    키순으로 세웠는데 앞 번호로 받고 싶어 짝꿍과 자리를 바꿨다
+    현수는 쩍꿍보다 키가 크다
+    정렬된 배열과 정렬되지 않은 배열을 비교해서 계산하
+    */
+
+  // 내가 푼 풀이
+  //   let answer = [];
+  //   const keyArr = [...arr];
+  //   arr.sort((a, b) => a - b);
+  //   const len = arr.length;
+
+  //   for (let i = 0; i < len; i++) {
+  //     if (keyArr[i] !== arr[i]) answer.push(i + 1);
+  //   }
+
+  return answer;
+}
+
+let arr = [120, 130, 150, 150, 130, 150];
+console.log(solution(arr));
