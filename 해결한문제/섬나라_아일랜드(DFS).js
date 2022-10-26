098b999e3b19283c2ac4aff20c996234edf23d98
bjc1102 <bjc30849@gmail.com>
2022-10-26 21:10:39 +0900

    DFS : 섬나라 아일랜드(DFS)

---
+++ 해결한문제/섬나라_아일랜드(DFS).js
@@ -0,0 +1,43 @@
+function solution(board) {
+  let answer = 0;
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  const dlen = dx.length;
+
+  function DFS(x, y) {
+    for (let i = 0; i < dlen; i++) {
+      const px = x + dx[i];
+      const py = y + dy[i];
+      // 하나라도 만족하지 않으면
+      if (px >= 0 && px < n && py >= 0 && py < n && board[px][py] === 1) {
+        board[px][py] = 0;
+        DFS(px, py);
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        DFS(i, j);
+        answer += 1;
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
