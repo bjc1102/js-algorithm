e6049135bf7add98f3b329e72dec87e3f843b8d8
bjc1102 <bjc30849@gmail.com>
2022-10-31 21:25:56 +0900

    스택: 올바른 괄호

--- 해결한문제/올바른괄호.js
+++ 해결한문제/올바른괄호.js
@@ -1,25 +1,15 @@
 function solution(s) {
-  let answer = "YES";
   const stack = [];
 
-  /*
- ( -> push
- ) -> pop
-    스택의 길이가 0이면 YES    
-*/
-
-  // (가 없는데 )가 있으면 안된다
-  // 만약 )가 더 많으면?..
-
-  // 다 생각했는데 구현하려는 생각이 부족했다.
-  for (const char of s) {
-    if (char === "(") stack.push(char);
-    else {
-      if (stack.pop() === undefined) return "NO";
+  for (const x of s) {
+    if (x === ")") {
+      const pop = stack.pop();
+      if (!pop) return false;
+    } else {
+      stack.push(x);
     }
   }
-  if (stack.length !== 0) answer = "NO";
-  return answer;
+  return stack.length === 0 ? true : false;
 }
 
 let a = "(()(()))(()";
