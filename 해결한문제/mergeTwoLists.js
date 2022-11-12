505ad1c584c92716b99bd65044de0d63677dd353
bjc1102 <bjc30849@gmail.com>
2022-11-12 22:54:40 +0900

    reverseList

---
+++ 해결한문제/mergeTwoLists.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ *
+ * 두가지 노드 리스트를 정렬해서 반환하기
+ */
+var mergeTwoLists = function (list1, list2) {
+  if (!list1 || !list2) return list1 || list2;
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  }
+  list2.next = mergeTwoLists(list1, list2.next);
+  return list2;
+};
