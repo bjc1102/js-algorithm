9e76e70e411104a870dc57f991b8a4cc2c980ab4
bjc1102 <bjc30849@gmail.com>
2022-12-05 22:01:01 +0900

    이진덧셈

---
+++ 해결한문제/Add_Binary.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  // 이진덧셈 구현하기
+  // const sum = parseInt(a, 2) + parseInt(b, 2);
+  // return sum.toString(2);
+  //   return (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
+
+  // Truth Table
+  // 1st + 2nd + carry = sum, new carry, decimal sum
+  //   0 +  0  + 0 = 0, 0 (0)
+  //   0 +  0  + 1 = 1, 0 (1)
+  //   0 +  1  + 1 = 0, 1 (2)
+  //   1 +  1  + 1 = 1, 1 (3)
+
+  let carry = 0;
+  let result = "";
+
+  let len1 = a.length - 1;
+  let len2 = b.length - 1;
+
+  for (; len1 >= 0 || len2 >= 0 || carry > 0; len1--, len2--) {
+    let sum = (+a[len1] || 0) + (+b[len2] || 0) + carry;
+    if (sum > 1) {
+      sum = sum % 2;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    result = `${sum}${result}`;
+  }
+  return result;
+};
