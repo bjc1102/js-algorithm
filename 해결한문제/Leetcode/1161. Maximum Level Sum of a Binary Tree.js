c110bd3680dc22d546cf0541a9b4ffb64855d977
bjc1102 <bjc30849@gmail.com>
2023-07-08 22:10:51 +0900

    BFS: 1161. Maximum Level Sum of a Binary Tree

--- 해결한문제/Leetcode/1161. Maximum Level Sum of a Binary Tree.js
+++ 해결한문제/Leetcode/1161. Maximum Level Sum of a Binary Tree.js
@@ -13,23 +13,32 @@
  각 level에 있는 모든 노드의 합 중 가장 큰 합을 갖고있는 level을 리턴해라
  */
 var maxLevelSum = function (root) {
-  if (root.length === 1) return root.val;
-  let max = root.val;
+  if (!root) return 0;
+
+  let maxSum = root.val;
   const queue = [root];
-  const level = [];
+  let level = 1;
+  let maxLevel = 1;
 
   while (queue.length > 0) {
-    const len = queue.length;
-    let sum = 0;
-    for (let i = 0; i < len; i++) {
-      const node = queue.shift();
-      sum += node.val;
+    const levelSize = queue.length;
+    let levelSum = 0;
 
-      if (node.left !== null) queue.push(node.left);
-      if (node.right !== null) queue.push(node.right);
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      levelSum += node.val;
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
     }
-    level.push(sum);
-    max = Math.max(max, sum);
+
+    if (levelSum > maxSum) {
+      maxSum = levelSum;
+      maxLevel = level;
+    }
+
+    level++;
   }
-  return level.indexOf(max) + 1;
+
+  return maxLevel;
 };
