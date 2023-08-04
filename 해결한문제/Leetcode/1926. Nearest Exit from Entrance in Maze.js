830ff7fb2ee3e56a8a7774e19ded3a03a3f300cd
bjc1102 <bjc30849@gmail.com>
2023-08-04 22:24:55 +0900

    bfs: 1926. Nearest Exit from Entrance in Maze 변수명 수정

--- 해결한문제/Leetcode/1926. Nearest Exit from Entrance in Maze.js
+++ 해결한문제/Leetcode/1926. Nearest Exit from Entrance in Maze.js
@@ -5,10 +5,14 @@
  * 상하좌우로 움직일 수 있다.
  * 이때 가장 가까운 출구를 찾아라
  * 벽(+) 빈셀(.)으로 표현된다.
+ *
+ * x와 y라서 너무 헷갈렸다
+ * nx의 범위가 x축 범위인줄 알았는데 실제 구현할 땐 y축 범위로 구현해야했다.
+ * 왜냐하면 [][] 2차원 배열에서 x축으로 움직이는 범위는 행렬로만 따지면 [y][x]이다.
  */
 var nearestExit = function (maze, entrance) {
-  const x = [-1, 0, 1, 0];
-  const y = [0, -1, 0, 1];
+  const dirm = [-1, 0, 1, 0];
+  const dirn = [0, -1, 0, 1];
   let level = 1;
 
   const queue = [entrance];
@@ -18,28 +22,28 @@ var nearestExit = function (maze, entrance) {
     const len = queue.length;
 
     for (let i = 0; i < len; i++) {
-      const [pointX, pointY] = queue.shift();
+      const [pm, pn] = queue.shift();
 
-      for (let j = 0; j < x.length; j++) {
-        const nx = pointX + x[j];
-        const ny = pointY + y[j];
+      for (let j = 0; j < 4; j++) {
+        const nm = pm + dirm[j];
+        const nn = pn + dirn[j];
 
         if (
-          nx >= 0 &&
-          nx < maze.length &&
-          ny >= 0 &&
-          ny < maze[0].length &&
-          maze[nx][ny] === "."
+          nm >= 0 &&
+          nm < maze.length &&
+          nn >= 0 &&
+          nn < maze[0].length &&
+          maze[nm][nn] === "."
         ) {
           if (
-            nx === 0 ||
-            ny === 0 ||
-            nx === maze.length - 1 ||
-            ny === maze[0].length - 1
+            nm === 0 ||
+            nn === 0 ||
+            nm === maze.length - 1 ||
+            nn === maze[0].length - 1
           )
             return level;
-          maze[nx][ny] = "-";
-          queue.push([nx, ny]);
+          maze[nm][nn] = "-";
+          queue.push([nm, nn]);
         }
       }
     }
